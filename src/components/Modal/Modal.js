df1970c65f6813a9f39d852d0289930cd58586b5
Yoann <maulineyoann@gmail.com>
2022-06-19 00:58:26 +0200

    fix css

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -3,10 +3,11 @@ import { Link } from 'react-router-dom'
 import './Modal.scss'
 
 const Modal = ({score}) => {
+    const points = score > 1 ? 'points' : 'point'
   return (
     <section className='modal'>
         <div className="content">
-            <h1>Bravo vous avez terminer le quizz avec <span className='score'>{score}</span> points</h1>
+            <h1>Bravo vous avez terminer le quizz avec <span className='score'>{score}</span> {points}</h1>
             <button>
                 <Link to='/'>Rejouer</Link>
             </button>
